d7db97e47c261e67e453574070931d6f41cf4589
caesarbriones43 <caesarbriones43@gmail.com>
2022-03-22 01:59:05 -0600

    creating a listview

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ListView1Screen extends StatelessWidget {
+  const ListView1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("ListView1"),
+        ),
+        body: ListView(
+          children: [
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+            const Text("data"),
+          ],
+        ));
+  }
+}
