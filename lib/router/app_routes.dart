1641cde27ec4e380c2586e1ddac88e8e70aa7f0b
caesarbriones43 <caesarbriones43@gmail.com>
2022-03-27 03:16:18 -0600

    Navigation to new screen; dynamic routes

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_components/models/models.dart';
 
 import '../screens/screen.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(
+        route: 'home',
+        name: 'HomeScreen',
+        icon: Icons.home,
+        screen: const HomeScreen()),
+    MenuOption(
+        route: 'listview1',
+        name: 'ListView1Screen',
+        icon: Icons.list,
+        screen: const HomeScreen()),
+    MenuOption(
+        route: 'listview2',
+        name: 'ListView2Screen',
+        icon: Icons.list,
+        screen: const ListView2Screen()),
+    MenuOption(
+        route: 'card',
+        name: 'CardScreen',
+        icon: Icons.list,
+        screen: const CardScreen()),
+    MenuOption(
+        route: 'alert',
+        name: 'AlertScreen',
+        icon: Icons.list,
+        screen: const AlertScreen())
+  ];
 
   static Route<dynamic> onGenerateRoute(settings) {
     return MaterialPageRoute(builder: (context) => const AlertScreen());
