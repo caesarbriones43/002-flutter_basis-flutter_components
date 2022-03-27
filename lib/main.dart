ca4b991fa760cb22ea3904a051f521141cc5a32d
caesarbriones43 <caesarbriones43@gmail.com>
2022-03-27 01:56:52 -0600

    Homescreen & initial routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/screens/listview2_screen.dart';
+import 'package:flutter_components/screens/screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,9 +8,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        home: ListView2Screen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => const HomeScreen(),
+        'listview1': (BuildContext context) => const ListView1Screen(),
+        'listview2': (BuildContext context) => const ListView2Screen(),
+        'card': (BuildContext context) => const CardScreen(),
+        'alert': (BuildContext context) => const AlertScreen(),
+      },
+    );
   }
 }
